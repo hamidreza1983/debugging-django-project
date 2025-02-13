8e2b36798c979b01fab1d259df6463451634e514
YVismeh <Yvisme41148@gmail.com>
2025-02-13 04:31:58 -0600

    team

--- team/views.py
+++ team/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+from .models import Team
 
 # Create your views here.
 
-def teams(request):
-    pass
+class TeamsView(TemplateView):
+    template_name = 'team/team.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["teams"] = Team.objects.filter(status=True)
+        return context
