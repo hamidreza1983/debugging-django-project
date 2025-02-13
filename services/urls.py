4017f234e16549623c7cec16f9ec9ef6ea316bab
bugnaslZ <bugnaslz83@gmail.com>
2025-02-13 11:39:10 -0800

    api

--- services/urls.py
+++ services/urls.py
@@ -1,8 +1,9 @@
-from django.urls import path
+from django.urls import path,include
 from .views import ServicesView
 
 app_name = "services"
 
 urlpatterns = [
     path("", ServicesView.as_view(), name="services"),
+    path('api/v1/',include('services.api.v1.urls')),
 ]
